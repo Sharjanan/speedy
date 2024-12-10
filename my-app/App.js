511cfaad72919f50845374d81828d57d88ea5af6
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2024-12-10 00:10:47 -0500

    tailwindcss configuration

--- my-app/App.js
+++ my-app/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import './tailwind.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import Navigator from './navigation/Navigator';
+import { Button } from '@material-tailwind/react';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Navigator />
+        <Navigator />
+        <Button >Material Tailwind Button</Button>
     </Provider>
   );
 }
