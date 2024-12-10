ed356e55457a76bfd4234699e6d23d93ecb2a916
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2024-12-10 02:08:05 -0500

    Navbar added Tailwind css #3

--- my-app/App.js
+++ my-app/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './tailwind.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import Navigator from './navigation/Navigator';
-import { Button } from '@material-tailwind/react';
+import { StickyNavbar } from './navigation/StickyNavbar';
+
 
 export default function App() {
   return (
     <Provider store={store}>
-        <Navigator />
-        <Button >Material Tailwind Button</Button>
+        <StickyNavbar />
     </Provider>
   );
 }
