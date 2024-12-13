d2b2c15c3cc96c5ef6e8d9ad9e5b396d459eb4f9
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2024-12-13 00:15:17 -0500

    navbar completed #3 *also created a home screen

--- my-app/App.js
+++ my-app/App.js
@@ -3,12 +3,16 @@ import './tailwind.css';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { StickyNavbar } from './navigation/StickyNavbar';
+import  HomeScreen  from './screens/HomeScreen';
 
 
 export default function App() {
   return (
     <Provider store={store}>
+       <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
         <StickyNavbar />
+        <HomeScreen />
+       </div>
     </Provider>
   );
 }
--- my-app/navigation/StickyNavbar.js
+++ my-app/navigation/StickyNavbar.js
@@ -41,23 +41,32 @@ export function StickyNavbar() {
           CONTACT
         </a>
       </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          ABOUT US
+        </a>
+      </Typography>
     </ul>
   );
  
   return (
-    <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
-      <Navbar className="sticky top-0 z-10 h-40 max-w-full rounded-none px-4 py-0 lg:px-8 lg:py-0">
+      <Navbar className="sticky top-0 z-10 h-32 max-w-full rounded-none px-12 py-0 lg:px-8 lg:py-0">
       <div className="flex items-center justify-between text-blue-gray-900">
       <div className="flex items-center justify-center lg:justify-start w-full lg:w-auto">
           <img
             alt="Logo"
-            className="ml-4 cursor-pointer "
+            className="lg:ml-4 cursor-pointer mt-[-50px]"
             src="../assets/pneuspeedy-removebg-preview.png"
-            style={{width: '200px', height: 'auto'}}
+            // style={{width: 'auto', height: '100%'}}
           />
         </div>
-          <div className="flex items-center gap-4 ">
-            <div className="mr-4 hidden lg:block ">{navList}</div>
+          <div className="flex items-center mt-[-50px] gap-4 ">
+            <div className=" mr-40 hidden lg:block ">{navList}</div>
             <div className="flex items-center gap-x-1">
               <Button
                 variant="text"
@@ -76,7 +85,7 @@ export function StickyNavbar() {
             </div>
             <IconButton
               variant="text"
-              className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+              className=" h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
               ripple={false}
               onClick={() => setOpenNav(!openNav)}
             >
@@ -125,33 +134,7 @@ export function StickyNavbar() {
           </div>
         </MobileNav>
       </Navbar>
-      <div className="mx-auto max-w-screen-md py-12">
-        <Card className="mb-12 overflow-hidden">
-          <img
-            alt="nature"
-            className="h-[32rem] w-full object-cover object-center"
-            src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
-          />
-        </Card>
-        <Typography variant="h2" color="blue-gray" className="mb-2">
-          What is Material Tailwind
-        </Typography>
-        <Typography color="gray" className="font-normal">
-          Can you help me out? you will get a lot of free exposure doing this
-          can my website be in english?. There is too much white space do less
-          with more, so that will be a conversation piece can you rework to make
-          the pizza look more delicious other agencies charge much lesser can
-          you make the blue bluer?. I think we need to start from scratch can my
-          website be in english?, yet make it sexy i&apos;ll pay you in a week
-          we don&apos;t need to pay upfront i hope you understand can you make
-          it stand out more?. Make the font bigger can you help me out? you will
-          get a lot of free exposure doing this that&apos;s going to be a chunk
-          of change other agencies charge much lesser. Are you busy this
-          weekend? I have a new project with a tight deadline that&apos;s going
-          to be a chunk of change. There are more projects lined up charge extra
-          the next time.
-        </Typography>
-      </div>
-    </div>
+    
+
   );
 }
