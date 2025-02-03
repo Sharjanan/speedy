50b0e74150279e481190447b859b2f4fca2652c5
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2025-02-03 03:02:51 -0500

    added scrolling effect when clicked call back #5

--- my-app/screens/HomeScreen.js
+++ my-app/screens/HomeScreen.js
@@ -1,14 +1,27 @@
-import React from "react";
-import { Button, Typography, Card } from "@material-tailwind/react";
-import { RequestForm }from"../components/RequestForm"
-import { Welcome }from"../components/Welcome"
-import { NewsLetter3 }from"../components/NewsLetter3"
+import React, { useRef } from "react";
+import { Typography } from "@material-tailwind/react";
+import { RequestForm } from "../components/RequestForm";
+import { Welcome } from "../components/Welcome";
+import { NewsLetter3 } from "../components/NewsLetter3";
+
 const HomeScreen = () => {
+  // Create a reference for the RequestForm section
+  const requestFormRef = useRef(null);
+
+  // Function to scroll to the form section
+  const scrollToRequestForm = () => {
+    if (requestFormRef.current) {
+      requestFormRef.current.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   return (
     <div>
       <div className="mx-auto">
-        <Welcome />
-        {/* New Section */}
+        {/* Pass scrollToRequestForm function as a prop to Welcome */}
+        <Welcome onRequestCallback={scrollToRequestForm} />
+
+        {/* "HOW IT WORKS" Section */}
         <div className="bg-black py-12">
           <div className="mx-auto max-w-screen-lg text-center">
             <Typography variant="h1" className="text-white text-4xl font-bold">
@@ -18,26 +31,19 @@ const HomeScreen = () => {
         </div>
       </div>
 
-      <div className=" mx-auto max-w-screen-md">
+      <div className="mx-auto max-w-screen-md">
         <Typography variant="h2" color="blue-gray" className="mb-2">
-          What is Material Tailwind
+          What is Material Tailwind?
         </Typography>
         <Typography color="gray" className="font-normal">
-          Can you help me out? you will get a lot of free exposure doing this
-          can my website be in english?. There is too much white space do less
-          with more, so that will be a conversation piece can you rework to make
-          the pizza look more delicious other agencies charge much lesser can
-          you make the blue bluer?. I think we need to start from scratch can my
-          website be in english?, yet make it sexy i&apos;ll pay you in a week
-          we don&apos;t need to pay upfront i hope you understand can you make
-          it stand out more?. Make the font bigger can you help me out? you will
-          get a lot of free exposure doing this that&apos;s going to be a chunk
-          of change other agencies charge much lesser. Are you busy this
-          weekend? I have a new project with a tight deadline that&apos;s going
-          to be a chunk of change. There are more projects lined up charge extra
-          the next time.
+          Our service allows you to book tire changes at your convenience...
         </Typography>
-        <RequestForm />
+
+        {/* Wrap RequestForm in a div and attach ref */}
+        <div ref={requestFormRef}>
+          <RequestForm />
+        </div>
+
         <NewsLetter3 />
       </div>
     </div>
