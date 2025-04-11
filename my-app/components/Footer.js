f30336f1a22554d475bd0ff6cb5f4a441045f05b
Sharjanan <98242877+Sharjanan@users.noreply.github.com>
2025-04-11 02:14:36 -0400

    Footer error Class->className #5

--- my-app/components/Footer.js
+++ my-app/components/Footer.js
@@ -16,15 +16,15 @@ const currentYear = new Date().getFullYear();
 
 export function Footer() {
   return (
-    <footer class="w-full bg-white p-8">
-  <div class="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
-  <img src="../assets/pneuspeedy-removebg-preview.png" alt="Custom Image" class=" object-contain" />
+    <footer className="w-full bg-white p-8">
+  <div className="flex flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 bg-white text-center md:justify-between">
+  <img src="../assets/pneuspeedy-removebg-preview.png" alt="Custom Image" className=" object-contain" />
  
-    <ul class="flex flex-wrap items-center gap-y-2 gap-x-8">
+    <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
       <li>
         <a
           href="#"
-          class="text-slate-700 hover:text-slate-500 focus:text-slate-500 text-sm"
+          className="text-slate-700 hover:text-slate-500 focus:text-slate-500 text-sm"
         >
           About Us
         </a>
@@ -32,7 +32,7 @@ export function Footer() {
       <li>
         <a
           href="#"
-          class="text-slate-700 hover:text-slate-500 focus:text-slate-500 text-sm"
+          className="text-slate-700 hover:text-slate-500 focus:text-slate-500 text-sm"
         >
           Privacy Policy
         </a>
@@ -40,14 +40,14 @@ export function Footer() {
       <li>
         <a
           href="#"
-          class="text-slate-700 hover:text-slate-500 focus:text-slate-500 text-sm"
+          className="text-slate-700 hover:text-slate-500 focus:text-slate-500 text-sm"
         >
           Contact Us
         </a>
       </li>
     </ul>
   </div>
-  <p class="block mb-4 text-sm text-center text-slate-500 md:mb-0 border-t border-slate-200 mt-4 pt-4">
+  <p className="block mb-4 text-sm text-center text-slate-500 md:mb-0 border-t border-slate-200 mt-4 pt-4">
     Copyright © 2024&nbsp; 
     <a href="https://material-tailwind.com/" target="_blank" rel="noreferrer">Pneu Speedy Mobile</a>.
   </p>
